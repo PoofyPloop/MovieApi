62b638bd367dc2fca0e8703394361ebc6bf2e099
Poofs <77813904+PoofyPloop@users.noreply.github.com>
2023-01-04 20:55:19 -0500

    committing files

---
+++ client.js
@@ -0,0 +1,92 @@
+// StAuth10222: I Rawad Haddad, 000777218 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
+
+const { default: axios } = require("axios")
+
+// post request 1
+const postRequestOne = async () => {
+    try {
+        const firstPost = await axios.post('http://localhost:3000/api', {
+            title: 'The Terminator',
+            release_year: '1984',
+            time_viewed: '2002-10-05T02:20:45.300'
+        });
+        console.log(firstPost.data);
+    } catch (error) {
+        // Handle Error Here
+        console.error(error);
+    }
+};
+
+// post request 2
+const postRequestTwo = async () => {
+    try {
+        const secondPost = await axios.post('http://localhost:3000/api', {
+            title: 'The dorpy',
+            release_year: '1983',
+            time_viewed: '2012-10-05T02:20:45.300'
+        });
+        console.log(secondPost.data);
+    } catch (error) {
+        // Handle Error Here
+        console.error(error);
+    }
+};
+
+// get request
+async function getData() {
+    try {
+        const response = await axios.get('http://localhost:3000/api');
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Put request
+const putRequest = async () => {
+    try {
+        const resp = await axios.put('http://localhost:3000/api', [{title:"Gladiator", release_year:"2000", time_viewed:"2017-10-03T11:45:56.200"},{title:"Avengers: Infinity War", release_year:"2019",time_viewed:"2019-12-03T15:20:20.200"},{title:"Wonder Woman", release_year:"2017",time_viewed:"2017-06-04T08:45:56.200"},{title: 'The Terminator', release_year: '1984', time_viewed: '2002-10-05T02:20:45.300'}]);
+        console.log(resp.data);
+    } catch (err) {
+        // Handle Error Here
+        console.error(err);
+    }
+};
+
+// delete item request
+const deleteItemRequest = async () => {
+    try {
+        const resp = await axios.delete('http://localhost:3000/api/1/');
+        console.log(resp.data);
+    } catch (err) {
+        // Handle Error Here
+        console.error(err);
+    }
+};
+
+// delete request
+const deleteRequest = async () => {
+    try {
+        const resp = await axios.delete('http://localhost:3000/api/');
+        console.log(resp.data);
+    } catch (err) {
+        // Handle Error Here
+        console.error(err);
+    }
+};
+
+const testOutput = async () => {
+    console.log("Test #1");
+    await postRequestOne();
+    await postRequestTwo();
+    await getData();
+    console.log("\nTest #2");
+    await putRequest();
+    await getData();
+    await deleteItemRequest();
+    await getData();
+    await deleteRequest();
+    await getData();
+};
+
+testOutput();
---
+++ server.js
@@ -0,0 +1,162 @@
+// StAuth10222: I Rawad Haddad, 000777218 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
+
+const sqlite3 = require("sqlite3").verbose();
+const sqlite = require("sqlite");
+const express = require("express");
+const app = express();
+
+// use JSON middleware to parse request bodies and put result into req.body
+app.use(express.json());
+
+/** Collection */
+
+// GET the entire collection, send it back as JSON data
+app.get("/api", async function (req, res) {
+  // get the data to be sent back
+  const data = await db.all(
+    "SELECT * FROM Movies"
+  );
+
+  // send back table data as JSON data
+  res.json(data);
+});
+
+// PUT the entire collection, send it back as JSON data
+app.put("/api", async function (req, res) {
+  // delete all current data in the collection
+  let data = await db.run(
+    "DELETE FROM Movies"
+  );
+  // data array
+  let output = [];
+  // loop that inserts the new data array
+  for (const [i, obj] of req.body.entries()) {
+    let insert = `INSERT INTO "Movies" (`;
+  let values = "VALUES( "
+  // data loop that sets the value to it's key
+  for (const [index, [key, value]] of Object.entries(Object.entries(obj))) {
+    if (index == 0) {
+      insert = insert.concat(`${key}`)
+      values = values.concat(`"${value}"`)
+    }
+    else {
+      insert = insert.concat(`,${key}`)
+      values = values.concat(`,"${value}"`)
+    }
+  }
+  const data = await db.run(
+    `${insert}) ${values})`
+  );
+  console.log(data);
+  output = [...output,data]
+  }
+  console.log(output);
+
+  // send back table data as JSON data
+  res.json({status: "REPLACE COLLECTION SUCCESSFUL"});
+});
+
+// POST the entire collection, send it back as JSON data
+app.post("/api", async function (req, res) {
+  // sql syntax variables
+  let insert = `INSERT INTO "Movies" (`;
+  let values = "VALUES( "
+  // item adding loop with values set on appropriate keys
+  for (const [index, [key, value]] of Object.entries(Object.entries(req.body))) {
+    if (index == 0) {
+      insert = insert.concat(`${key}`)
+      values = values.concat(`"${value}"`)
+    }
+    else {
+      insert = insert.concat(`,${key}`)
+      values = values.concat(`,"${value}"`)
+    }
+  }
+  console.log("insert and value: ", `${insert}) ${values})`)
+  const data = await db.run(
+    `${insert}) ${values})`
+  );
+
+  // send back table data as JSON data
+  res.json({status: "CREATE ENTRY SUCCESSFUL"});
+});
+
+// DELETE the entire collection, send it back as JSON data
+app.delete("/api", async function (req, res) {
+  // get the data to be sent back
+  const data = await db.run(
+    "DELETE FROM Movies"
+  );
+
+  // send back table data as JSON data
+  res.json({status: "DELETE COLLECTION SUCCESSFUL"});
+});
+
+/** Individual */
+
+// GET a single item, send it back as JSON data
+app.get("/api/:id", async function (req, res) {
+  // get the data to be sent back
+  console.log("Req result: ", req.params.id);
+  const data = await db.all(
+    "SELECT rowid, title, release_year, time_viewed FROM Movies WHERE rowid=?", req.params.id
+  );
+
+  // send back table data as JSON data
+  res.json(data);
+});
+
+// PUT a single item, send it back as JSON data
+app.put("/api/:id", async function (req, res) {
+  let set = "SET "
+  // data loop that sets the value to it's key
+  for (const [index, [key, value]] of Object.entries(Object.entries(req.body))) {
+    x =`${key}="${value}"`;
+    if (index == 0) {
+      set = set.concat(`${x}`)
+    }
+    else {
+      set = set.concat(`,${x}`)
+    }
+  }
+  console.log(`UPDATE Movies ${set} WHERE rowid=${req.params.id}` )
+  // updating the item with the data from the loop
+  const data = await db.run(
+    `UPDATE "Movies" ${set} WHERE rowid=${req.params.id}`
+  );
+
+  // send back table data as JSON data
+  res.json({status: "UPDATE ITEM SUCCESSFUL"});
+});
+
+// DELETE a single item, send it back as JSON data
+app.delete("/api/:id", async function (req, res) {
+  // delete item
+  const data = await db.run(
+    "DELETE FROM Movies WHERE rowid=?", req.params.id
+  );
+  // send back table data as JSON data
+  res.json({status: "DELETE ITEM SUCCESSFUL"});
+});
+
+// creates the database and table of data to be managed, then starts the server
+async function startup() {
+  // create the database connection
+  db = await sqlite.open({
+    filename: "api.db",
+    driver: sqlite3.Database,
+  });
+
+  // create
+  await db.run("DROP TABLE IF EXISTS Movies");
+  await db.run(
+    "CREATE TABLE Movies (title TEXT, release_year TEXT, time_viewed TEXT)"
+  );
+
+  // start the server
+  const server = app.listen(3000, function () {
+    console.log("RESTful API listening on port 3000!");
+  });
+}
+
+startup();
